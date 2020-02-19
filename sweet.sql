34dc3f7aa3ce3cccd415dfed5c326fed41bc0833
Mops04-mop <61012573+Mops04-mop@users.noreply.github.com>
2020-02-19 15:31:57 +0200

    Add files via upload

---
+++ sweet.sql
@@ -0,0 +1,6 @@
+type,price,brand,stock,barcode,quantity,id
+Amajoya,R12,Amajoya,50,15001543640,125g,1
+Jelly-Babies,R13,Maynatts,50,14015400641,125g,2
+SoftGums,R13,"Mr Sweets",50,11015400643,125g,3
+Winegums,R14,Beacon,50,13001540644,125g,4
+Mentos,R12,Mentos,50,120458000645,125g,5
