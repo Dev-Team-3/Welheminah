7914e4d26f49db85b0989485f74b0118ff1bca14
Mops04-mop <61012573+Mops04-mop@users.noreply.github.com>
2020-02-19 11:50:21 +0200

    Add files via upload

---
+++ book.sql
@@ -0,0 +1,6 @@
+type,price,brand,stock,barcode,quantity,id
+Coke,R15,Coke,NULL,NULL,NULL,NULL
+Fanta,R11,Coke,NULL,NULL,NULL,NULL
+Sprite,R12,Coke,NULL,NULL,NULL,NULL
+"Dry Lemon",R14,Sweppes,NULL,NULL,NULL,NULL
+Stoney,R10,Coke,NULL,NULL,NULL,NULL
